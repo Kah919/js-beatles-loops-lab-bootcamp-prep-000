fcb0e2b213fa13aa0ff728ebd3de0628fc9b2898
Kah Yap <kahyap13@gmail.com>
2018-10-24 07:10:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-// add solution
-function theBeatlesPlay(musicians, instruments) {
-  var empty = [];
-  for(var i = 0; i < musicians.length; i++) {
-    empty.push(`${musicians[i]} plays ${instruments[i]}`)
-  }
-  return empty;
-}
+// add solution here
