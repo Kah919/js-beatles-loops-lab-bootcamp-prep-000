14ebc2bbdd391c7e14b6df4f861bf165cefb5258
Kah Yap <kahyap13@gmail.com>
2018-10-24 07:28:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function johnLennonFacts(x) {
 function iLoveTheBeatles(num) {
   var empty = [];
   do{
-    empty.push("I love the Beatles!")
+    empty.push("I love the Beatles!");
     num++;
-  }while(num < 15)
+  }while(num < 15);
   return empty;
 }
